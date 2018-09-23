33bc647d8e57aaa91fef707c0592db4208693bb7
Takahashi, Takeru <tkrtkhsh@gmail.com>
2018-09-23 15:39:15 +0900

    add: task から編集/削除の ActionSheet を表示

--- app/src/pages/keep/keep.ts
+++ app/src/pages/keep/keep.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
 
 /**
  * Generated class for the KeepPage page.
@@ -16,7 +16,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class KeepPage {
 
   keep = {};
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public actionSheetCtrl: ActionSheetController) {
     this.keep = navParams.get('keep');
     this.keep['description'] = '色は暖色系の色味ではなかったけれど、家族・旦那さんが好きな色味でデザインとボリュームが今まで着たドレスの中で1番'
   }
@@ -25,4 +25,26 @@ export class KeepPage {
     console.log('ionViewDidLoad PanelPage');
   }
 
+  showMenu() {
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: '編集',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: '削除',
+          role: 'destructive',
+          handler: () => {
+            //todo
+          }
+        },{
+          text: 'キャンセル',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }
