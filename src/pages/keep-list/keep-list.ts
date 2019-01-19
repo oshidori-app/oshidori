35ee64506c4f7ade0a22a7d316091b2ee2f05bf3
樋口亮 <>
2019-01-20 00:11:47 +0900

    画像を取得して表示できるようになた

--- src/pages/keep-list/keep-list.ts
+++ src/pages/keep-list/keep-list.ts
@@ -18,7 +18,7 @@ import { KeepPage } from '../keep/keep';
  * on Ionic pages and navigation.
  */
 
-export class KeepListVm {
+export class KeepVm {
   title?: string
   imgUrl?: string
   downloadUrl?: Observable<string>
@@ -41,7 +41,10 @@ export class KeepListPage {
   // public taglist = [];
   public task;
 
-  public keepListVms: KeepListVm[];
+  public keepVmInDL: KeepVm[];
+  public keepListVm: (KeepVm[])[];
+
+  // public keepListVm: keepListVms[];
 
   constructor(
     public navCtrl: NavController,
@@ -61,10 +64,6 @@ export class KeepListPage {
     });
     this.keepRepo.add(keepReg)
       .then((ref) => {
-        // Logger.debug(ref);
-        // test.ref = ref;
-        // this.testRepo.update(test);
-        //this.dutil.showToast('登録しました');
       })
       .catch(err => {
         this.dutil.showToast(err);
@@ -74,66 +73,40 @@ export class KeepListPage {
   }
 
   private getKeeps() {
+
     let keep = new Keep();
     this.keepRepo.list(keep).subscribe(keepList => {
       Logger.debug(keepList);
-      this.keepListVms = keepList;
+
+      // 一旦keepVmInDLに格納して、downloadUrlを付与
+      this.keepVmInDL = keepList;
       keepList.forEach((keep, i) => {
         let imageURL = this.storage.getDownloadURL(keep.imgUrl);
-        this.keepListVms[i].downloadUrl = imageURL;
-        Logger.debug(imageURL);
-        Logger.debug(keep.imgUrl);
-        Logger.debug(this.keepListVms[i].downloadUrl);
+        this.keepVmInDL[i].downloadUrl = imageURL;
       });
+
+      // 表示用に配列を整形
+      let xNum = 2;
+      let ret = [];
+      for (let i = 0; i < Math.ceil(this.keepVmInDL.length / xNum); i++) {
+        var index = i * xNum;
+        ret.push(this.keepVmInDL.slice(index, index + xNum));
+      }
+      this.keepListVm = ret;
+      Logger.debug("aaaaa");
+      Logger.debug(this.keepListVm);
+
     });
   }
 
-  // ionViewDidLoad() {
-  //   this.addkeeps();
-  //   this.getKeeps();
-  // }
-
   ionViewWillEnter() {
     this.dutil.showLoader("データを読み込んでいます...");
-    this.addkeeps();
+    //this.addkeeps();
     this.getKeeps();
   }
 
-  // ionViewDidLoad() {
-  //   // タグリストのデータ設定
-  //   //this.taglist = ["#ウェディングドレス","#白","#レース","#1着目","#チャペル"];
-
-  //   //キープリストのデータ設定
-  //   let keeps = [
-  //       { title: "dress1", src: "assets/img/image01.jpg", class:"ribbon" },
-  //       { title: "dress2", src: "assets/img/image04.jpg", class:"noRibbon" },
-  //       { title: "dress3", src: "assets/img/image08.jpg", class: "noRibbon" },
-  //       { title: "dress4", src: "assets/img/image02.jpg", class: "noRibbon" },
-  //       { title: "dress9", src: "assets/img/image09.jpg", class: "noRibbon" },
-  //       { title: "dress7", src: "assets/img/image07.jpg", class: "noRibbon" },
-  //       { title: "dress1", src: "assets/img/image01.jpg", class: "noRibbon" },
-  //       { title: "dress2", src: "assets/img/image04.jpg", class: "noRibbon" },
-  //       { title: "dress3", src: "assets/img/image08.jpg", class: "noRibbon" },
-  //       { title: "dress4", src: "assets/img/image02.jpg", class: "noRibbon" },
-  //       { title: "dress9", src: "assets/img/image09.jpg", class: "noRibbon" },
-  //       { title: "dress7", src: "assets/img/image07.jpg", class: "noRibbon" }
-  //     ];
-
-  //   let xNum = 2;  
-  //       // 表示用に配列を整形
-  //       let ret = [];
-  //       for(let i = 0; i < Math.ceil(keeps.length / xNum); i++){
-  //         var index = i * xNum;
-  //         ret.push(keeps.slice(index, index + xNum));
-  //       }
-  //    this.keeplist = ret;
-
-  // }
-
   goToKeep(task) {
-    // this.navCtrl.push(KeepPage, { keep: task });
+    this.navCtrl.push(KeepPage, { keep: task });
   }
 }
-
-//export class SharedModule {}
 
