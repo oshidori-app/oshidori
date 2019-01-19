693cab7fdb7946ce95b414443a394fcbee7e3277
totoppi <totoppi64@gmail.com>
2019-01-19 18:53:09 +0900

    groupを作成するように

---
+++ src/repository/group.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BaseRepository } from './base.repository';
+import { Group } from '../models/group';
+
+@Injectable()
+export class GroupRepository extends BaseRepository {
+
+    constructor(injector: Injector) {
+        super(injector);
+     }
+
+    public add(group: Group): Promise<any> {
+        return super.addDocument(group);
+    }
+
+    public update(group: Group): Promise<any> {
+        return super.updateDocument(group);
+    }
+
+    public delete(group: Group): void {
+        throw new Error('not implemented');
+    }
+
+    public list(group: Group): Observable<Group[]> {
+        return super.listDocument(group) as Observable<Group[]>;
+    }
+}
