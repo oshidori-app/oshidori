02c81eb9a27c985b1f10a5d83811dcb0ca2c31fd
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-01-19 18:13:40 +0900

    get task sfrom Firestore🔥

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { TaskRepository } from './../../repository/task.repository';
 import { InputTaskPage } from './../input-task/input-task';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
@@ -8,7 +9,16 @@ import { AccountPage } from '../account/account';
 import { TestRegistrationPage } from '../test-registration/test-registration';
 import { TestListPage } from '../test-list/test-list';
 import { Logger } from '../../logger';
+import { DisplayUtilService } from '../../providers/display-util.service';
 
+export class TaskListVm {
+    id?:    Number; // todo guid にしたいなぁ
+    title?: string;
+    limit?: Date;
+    src?:   string;
+    status?: string; // todo boolean にしたいなぁ
+    assign?: string; // todo assignedTo にしたいなぁ
+}
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
@@ -16,60 +26,42 @@ import { Logger } from '../../logger';
 
 export class HomePage {
 
-    public tasks = [];
+    public taskListVms: TaskListVm[];
+    public formatedTaskList: any[];
     public taskInfoVisible = false;
     public platformWidth = 0;
 
-    constructor(public navCtrl: NavController, public platform: Platform) {
+    constructor(
+        public navCtrl: NavController,
+        public platform: Platform,
+        private taskRepo: TaskRepository,
+        private dutil: DisplayUtilService
+    ) {
         // デバイスの画面幅を取得
         this.platformWidth = platform.width();
-
-        // パネルのデータを設定
-        // 本来はAPIの返り値の取得処理
-        this.tasks = this.makeTasks();
     }
 
-    makeTasks() {
-        let xNum = 2;
-        let tasks = [
-            new Task(1, "dress", "yyyy/MM/dd", "assets/img/dress01.jpg", "finished", "shinpu"),
-            new Task(2, "present", "yyyy/MM/dd", "assets/img/present01.jpg", "finished", "shinro"),
-            new Task(3, "travel", "yyyy/MM/dd", "assets/img/travel01.jpg", "unfinished", "shinro"),
-            new Task(4, "food", "yyyy/MM/dd", null, "unfinished", "shinro"),
-            new Task(5, "ring", "yyyy/MM/dd", "assets/img/ring01.jpg", "finished", "shinpu"),
-            new Task(6, "place", "yyyy/MM/dd", "assets/img/place01.jpg", "unfinished", "shinpu"),
-            new Task(7, "tuxede", "yyyy/MM/dd", "assets/img/tuxede01.jpg", "finished", "shinro"),
-            new Task(8, "secret", "yyyy/MM/dd", null, "finished", "shinpu"),
-            new Task(9, "dress", "yyyy/MM/dd", "assets/img/dress01.jpg", "finished", "shinpu"),
-            new Task(10, "present", "yyyy/MM/dd", "assets/img/present01.jpg", "unfinished", "shinro"),
-            new Task(11, "travel", "yyyy/MM/dd", "assets/img/travel01.jpg", "unfinished", "shinro"),
-            new Task(12, "secret", "yyyy/MM/dd", null, "unfinished", "shinro"),
-            new Task(13, "ring", "yyyy/MM/dd", "assets/img/ring01.jpg", "unfinished", "shinpu"),
-            new Task(14, "place", "yyyy/MM/dd", "assets/img/place01.jpg", "unfinished", "shinpu"),
-            new Task(15, "tuxede", "yyyy/MM/dd", "assets/img/tuxede01.jpg", "unfinished", "shinro"),
-            new Task(16, "food", "yyyy/MM/dd", null, "unfinished", "shinro"),
-        ]
-        // 表示用に配列を整形
+    ionViewDidEnter() {
+        Logger.debug("ionViewWillEnter: HomePage");
+        this.dutil.showLoader("データを読み込んでいます...");
+        this.getTasks();
+    }
+
+    private getTasks() {
+        this.taskRepo.list(new Task()).subscribe(taskList => {
+            Logger.debug(taskList);
+            this.taskListVms = taskList;
+            this.formatedTaskList = this.formatedArrayForView(2, this.taskListVms)
+        })
+    }
+
+    formatedArrayForView(xNum: number, array: any[]) {
         let ret = [];
-        for (let i = 0; i < Math.ceil(tasks.length / xNum); i++) {
+        for (let i = 0; i < Math.ceil(array.length / xNum); i++) {
             var index = i * xNum;
-            ret.push(tasks.slice(index, index + xNum));
+            ret.push(array.slice(index, index + xNum));
         }
         return ret
-    }
-
-    ionViewDidLoad() {
-        Logger.debug("ionViewDidLoad: homepage")
-    }
-
-    taskInfoViewToggle() {
-        this.taskInfoVisible = !this.taskInfoVisible;
-        if (this.taskInfoVisible) {
-            [].forEach.call(document.getElementsByClassName('task_info'), function (taskinfo) {
-                taskinfo.classList.remove('fadeOutDown');
-                taskinfo.classList.add('fadeInUp');
-            });
-        }
     }
 
     goToKeepList(task) {
